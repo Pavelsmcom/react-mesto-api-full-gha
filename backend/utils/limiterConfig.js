14d6db163e437b0bf5de8ba9c81ef37e960238cf
Pavel Melekhin <79168445519@yandex.ru>
2023-04-02 18:21:24 +0300

    sprint15 v0.8

--- backend/utils/limiterConfig.js
+++ backend/utils/limiterConfig.js
@@ -1,5 +1,5 @@
 module.exports.limiterConfig = {
   windowMs: 1 * 60 * 1000, // 1  minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   message: 'to many request from this IP ',
 };
