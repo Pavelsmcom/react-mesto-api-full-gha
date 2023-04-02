fa77863b32cc594ea00f713f3e3140b5972ba0cf
Pavel Melekhin <79168445519@yandex.ru>
2023-04-02 14:14:19 +0300

    sprint15 v0.0

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const cardsRoutes = require('./routes/cards');
 const { login, createUser } = require('./controllers/user');
 
 const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minutes
+  windowMs: 1 * 60 * 1000, // 1  minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
   message: 'to many request from this IP ',
 });
